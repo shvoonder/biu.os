30d52b23cbff248eabf5a51ac1b1d7968fe6f277
Ronen <shvoonder@gmail.com>
2018-12-27 18:40:39 +0200

    waiting time added

--- Ex3/main.c
+++ Ex3/main.c
@@ -6,16 +6,21 @@ struct process{
     int pid;
     int burstTime;
     int arrTime;
+    int waitingTime;
 };
 
-void FCFS (struct process* prcsArr){
+void FCFS (struct process* prcsArr, int numOfProc){
     int i;
-    for (int i=0; i<sizeof(prcsArr); i++){
+    for (int i=0; i<numOfProc; i++){
         if (i > 0) //start checking from [1]
             if (prcsArr[i].arrTime < prcsArr[i-1].arrTime + prcsArr[i-1].burstTime) //check that start time isn't before previous ends
+                prcsArr[i].waitingTime = prcsArr[i].arrTime;
                 prcsArr[i].arrTime = prcsArr[i-1].arrTime + prcsArr[i-1].burstTime; //if it does, change start time
-        printf("#%d:[%d] - [%d]\n",prcsArr[i].pid, prcsArr[i].arrTime, prcsArr[i].arrTime + prcsArr[i].burstTime);
+                prcsArr[i].waitingTime = prcsArr[i].arrTime-prcsArr[i].waitingTime+prcsArr[i-1].arrTime+prcsArr[i-1].burstTime;
+        printf("#%d:[%d]-[%d]\n",prcsArr[i].pid, prcsArr[i].arrTime, prcsArr[i].arrTime + prcsArr[i].burstTime);
     }
+    for (i=0; i<numOfProc; i++)
+        printf("Process %d wiating time: %d", prcsArr[i].pid, prcsArr[i].waitingTime);
 }
 
 void RR (struct process prcsArr[]){
@@ -23,19 +28,19 @@ void RR (struct process prcsArr[]){
 }
 
 int main() {
-    printf("Enter amount of processes:");
+    printf("Enter amount of processes:\n");
     int numOfProc;
     int option;
     scanf("%d", &numOfProc);
     int i;
     struct process* processes = malloc(numOfProc*sizeof(int));
     for (i=0; i<numOfProc; i++) {
-        printf("Enter arrival time and burst time");
+        printf("Enter arrival time and burst time\n");
         scanf("%d %d", &processes[i].arrTime, &processes[i].burstTime);
-        processes[i].pid=i;
+        processes[i].pid=i+1;
     }
-    printf("Enter algorithm number:");
+    printf("Enter algorithm number:\n");
     scanf("%d", &option);
-
+    FCFS(processes, numOfProc);
     return 0;
 }
