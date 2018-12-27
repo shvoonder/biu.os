bd8463e0aaea7f9b5a337589dbe2db8c9919355c
Ronen <shvoonder@gmail.com>
2018-12-27 18:03:27 +0200

    fix

--- Ex3/main.c
+++ Ex3/main.c
@@ -8,30 +8,34 @@ struct process{
     int arrTime;
 };
 
-void FCFS (struct process prcsArr[]){
+void FCFS (struct process* prcsArr){
     int i;
     for (int i=0; i<sizeof(prcsArr); i++){
         if (i > 0) //start checking from [1]
             if (prcsArr[i].arrTime < prcsArr[i-1].arrTime + prcsArr[i-1].burstTime) //check that start time isn't before previous ends
                 prcsArr[i].arrTime = prcsArr[i-1].arrTime + prcsArr[i-1].burstTime; //if it does, change start time
-        printf("#%d:[%d] - [%d]\n",prcsArr->pid, prcsArr[i].arrTime, prcsArr[i].arrTime + prcsArr[i].burstTime);
+        printf("#%d:[%d] - [%d]\n",prcsArr[i].pid, prcsArr[i].arrTime, prcsArr[i].arrTime + prcsArr[i].burstTime);
     }
+}
+
+void RR (struct process prcsArr[]){
+
 }
 
 int main() {
     printf("Enter amount of processes:");
     int numOfProc;
     int option;
-    scanf("%d", numOfProc);
+    scanf("%d", &numOfProc);
     int i;
     struct process* processes = malloc(numOfProc*sizeof(int));
     for (i=0; i<numOfProc; i++) {
         printf("Enter arrival time and burst time");
-        scanf("%d %d", processes[i].arrTime, processes[i].burstTime);
+        scanf("%d %d", &processes[i].arrTime, &processes[i].burstTime);
         processes[i].pid=i;
     }
     printf("Enter algorithm number:");
-    scanf("%d", option);
-    
+    scanf("%d", &option);
+
     return 0;
 }
